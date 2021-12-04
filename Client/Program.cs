cf06b304a3aeadbcde01838ce27c7f7b645d9ab3
Vlad Gonzalez <vladimir.gonzalez@online.com.ni>
2021-12-04 12:23:34 -0600

    added blazor hosted wasm application

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,12 +3,13 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace UntoMeWorld.WASMClient
+namespace UntoMeWorld.WebClient.Client
 {
     public class Program
     {
@@ -17,11 +18,16 @@ namespace UntoMeWorld.WASMClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("UntoMeWorld.WebClient.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("UntoMeWorld.WebClient.ServerAPI"));
 
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://c3f5c1d4-0938-4f1c-bc87-6de6fdc96493/ServerAPI");
             });
 
             await builder.Build().RunAsync();
