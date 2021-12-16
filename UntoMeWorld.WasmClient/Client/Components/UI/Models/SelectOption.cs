03b6b8086ab41b3fccaf39acc347979fc8434274
Vladimir <vladimirgon151199@gmail.com>
2021-12-15 23:50:17 -0600

    added input and select controls and implemented them in the churches dialog

---
+++ UntoMeWorld.WasmClient/Client/Components/UI/Models/SelectOption.cs
@@ -0,0 +1,20 @@
+﻿namespace UntoMeWorld.WasmClient.Client.Components.UI.Models;
+
+public class SelectOption<T>
+{
+    public T Value { get; set; }
+    public string DisplayValue { get; set; }
+    
+    public bool Selected { get; set; }
+}
+
+public static class SelectOptionExtensions
+{
+    public static IEnumerable<SelectOption<TOption>> ToSelectOptions<T, TOption>(this IEnumerable<T> array,
+        Func<T, TOption> valueSelector, Func<T, string> displayNameSelector = null)
+        => array.Select(x => new SelectOption<TOption>
+        {
+            Value = valueSelector(x),
+            DisplayValue = displayNameSelector?.Invoke(x)
+        });
+}
