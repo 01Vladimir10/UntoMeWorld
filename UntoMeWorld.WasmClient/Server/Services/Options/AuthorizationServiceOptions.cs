2796d4e9cd9a291dc51e82704bd760395a18622a
Vladimir <vladimirgon151199@gmail.com>
2022-06-19 22:23:38 -0600

    removed unused properties from AuthorizationServiceOptions

--- UntoMeWorld.WasmClient/Server/Services/Options/AuthorizationServiceOptions.cs
+++ UntoMeWorld.WasmClient/Server/Services/Options/AuthorizationServiceOptions.cs
@@ -2,13 +2,6 @@
 
 public class AuthorizationServiceOptions
 {
-    public List<string> ReadActions { get; set; }
-    public List<string> DeleteActions { get; set; }
-    public List<string> UpdateActions { get; set; }
-    public List<string> AddActions { get; set; }
-    public List<string> RestoreActions { get; set; }
-    public List<string> PurgeActions { get; set; }
-    public List<string> SpecialActions { get; set; }
     public string PermissionSelectionMode { get; set; }
     public string RoleSelectionMode { get; set; }
 }
