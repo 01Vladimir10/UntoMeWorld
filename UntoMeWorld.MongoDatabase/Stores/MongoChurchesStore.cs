92c6b4b8871ac72d4ecec3e824e2fb11961b8da3
vladimir.gonzalez <vladimirgon151199@gmail.com>
2022-08-19 10:00:55 -0600

    simplified generic mongo store for churches

--- UntoMeWorld.MongoDatabase/Stores/MongoChurchesStore.cs
+++ UntoMeWorld.MongoDatabase/Stores/MongoChurchesStore.cs
@@ -1,21 +1,15 @@
 ﻿using UntoMeWorld.Domain.Model;
 using UntoMeWorld.Domain.Stores;
-using UntoMeWorld.MongoDatabase.ReadModels;
 using UntoMeWorld.MongoDatabase.Services;
 
 namespace UntoMeWorld.MongoDatabase.Stores
 {
-    public class MongoChurchesStore : GenericMongoStore<Church, Church>, IChurchesStore
+    public class MongoChurchesStore : GenericMongoStore<Church>, IChurchesStore
     {
         private const string ChurchesCollection = "churches";
 
         public MongoChurchesStore(MongoDbService service) :
-            base(service, ChurchesCollection, c =>
-            {
-                var church = c as Church;
-                church.Pastor = c.Pastor;
-                return church;
-            })
+            base(service, ChurchesCollection)
         {
             
         }
