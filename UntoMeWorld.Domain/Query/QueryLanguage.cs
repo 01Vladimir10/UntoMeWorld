da00011221630ddc01ff019aa12545a51fd819ff
vladimir.gonzalez <vladimirgon151199@gmail.com>
2022-11-24 15:38:29 -0600

    completed xmas labels

--- UntoMeWorld.Domain/Query/QueryLanguage.cs
+++ UntoMeWorld.Domain/Query/QueryLanguage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace UntoMeWorld.Domain.Query
 {
@@ -24,8 +25,10 @@ namespace UntoMeWorld.Domain.Query
 
         public static QueryFilter Eq(string property, IComparable value)
             => ComparableFilter(property, value, QueryOperator.Eq);
+
         public static QueryFilter Ne(string property, IComparable value)
             => ComparableFilter(property, value, QueryOperator.Ne);
+
         public static QueryFilter Lt(string property, IComparable value)
             => ComparableFilter(property, value, QueryOperator.Lt);
 
@@ -45,6 +48,35 @@ namespace UntoMeWorld.Domain.Query
                 Value = query
             };
 
+        public static QueryFilter Eq<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Eq);
+        public static QueryFilter Ne<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Ne);
+        public static QueryFilter Gt<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Gt);
+        public static QueryFilter Lt<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Lt);
+        public static QueryFilter Gte<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Gte);
+        public static QueryFilter Lte<T, TProp>(Expression<Func<T, TProp>> selector, TProp value)
+            where TProp : IComparable
+            => ComparableFilter(GetPropertyName(selector), value, QueryOperator.Lte);
+
+        private static string GetPropertyName<T, TProp>(Expression<Func<T, TProp>> selector)
+            => selector.Body switch
+            {
+                UnaryExpression unaryExpression => unaryExpression.Operand is MemberExpression expr
+                    ? expr.Member.Name
+                    : string.Empty,
+                MemberExpression memberExpression => memberExpression.Member.Name,
+                _ => string.Empty
+            };
+
         public static QueryFilter In<T>(string property, IEnumerable<T> items)
             => new()
             {
@@ -53,12 +85,12 @@ namespace UntoMeWorld.Domain.Query
                 Value = items
             };
 
-        private static QueryFilter ComparableFilter(string property, IComparable value, string op) 
+        private static QueryFilter ComparableFilter(string property, IComparable value, string op)
             => new()
-        {
-            PropertyName = property,
-            Value = value,
-            Operator = op
-        };
+            {
+                PropertyName = property,
+                Value = value,
+                Operator = op
+            };
     }
 }
---
+++ UntoMeWorld.WasmClient/Server/Controllers/ReportsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UntoMeWorld.Application.Services.Base;
+using UntoMeWorld.Domain.Model;
+using UntoMeWorld.Domain.Security;
+using UntoMeWorld.WasmClient.Server.Security.Authorization.Attributes;
+using static UntoMeWorld.Domain.Query.QueryLanguage;
+
+namespace UntoMeWorld.WasmClient.Server.Controllers;
+
+[Authorize]
+[ResourceName(ApiResource.LabelReports)]
+[Route("api/[controller]")]
+public class ReportsController : Controller
+{
+    private readonly IChildrenService _childrenService;
+
+    public ReportsController(IChildrenService childrenService)
+    {
+        _childrenService = childrenService;
+    }
+
+    [HttpGet("xmasLabels")]
+    public async Task<IActionResult> XmasReport() => View(await GetXmasChildren());
+
+    [HttpGet("xmasList")]
+    public async Task<IActionResult> XmasList() => View(await GetXmasChildren());
+
+    private async Task<List<Child>> GetXmasChildren()
+    {
+        var filter = And(
+            Eq((Child c) => c.IsDeleted, false),
+            Eq((Child c) => c.ReceivesChristmasGift, true)
+        );
+
+        var result = await _childrenService.Query(
+            filter: filter,
+            orderBy: nameof(Child.ChurchId),
+            orderDesc: true,
+            page: 1,
+            pageSize: 1000);
+        if (result.Result is null) return new List<Child>();
+        return result
+            .Result
+            .OrderBy(c => c.Church.Name)
+            .ThenBy(c => c.Grade >= 7 ? "HS" : "EL")
+            .ThenBy(c => c.Gender)
+            .ThenBy(c => c.Name)
+            .ToList();
+    }
+}
